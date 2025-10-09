190cfbc403c2f194964a08ac8c6d45fd151ee1a5
xyrionexx <aditcareer12370@gmail.com>
2025-10-09 10:12:19 +0700

    benarkan halaman login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,17 +3,17 @@ import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
+  pages: {
+    signOut: "/",  
+    signIn: "/login",
+    error: "/error",
+    verifyRequest: "/login",
+    newUser: "/login",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -21,7 +21,6 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  debug: true, // opsional, menampilkan log lebih detail di console
 });
 
 export { handler as GET, handler as POST };
